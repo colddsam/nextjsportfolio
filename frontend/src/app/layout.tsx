b3a4679d12a2fab04fc9b99f837c28430ff28a5c
colddsam <dassamratkumar772@gmail.com>
2024-06-09 17:11:54 +0530

    changing favicon

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,9 +1,13 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers";
 
-const inter = Inter({ subsets: ["latin"] });
+const inter = Roboto({
+  subsets: ["latin"],
+  weight: ['400', '700','500'],
+  style: ['normal', 'italic'],
+  display: 'swap',});
 
 export const metadata: Metadata = {
   title: "colddsam",
@@ -28,6 +32,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/images/profile/favicon.ico'" sizes="any" />
+        <link
+            rel="icon"
+            href="/icon?<generated>"
+            type="image/<generated>"
+            sizes="<generated>"
+        />
+      </head>
       <body className={inter.className}>
         <Providers>
           {children}
