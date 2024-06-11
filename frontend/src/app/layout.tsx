e8b0e60d7d5bd7227538a3b6e09f6868992f8b9c
colddsam <dassamratkumar772@gmail.com>
2024-06-11 07:31:03 +0530

    optimizing SEO

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -5,24 +5,57 @@ import { Providers } from "./providers";
 
 const inter = Roboto({
   subsets: ["latin"],
-  weight: ['400', '700','500'],
-  style: ['normal', 'italic'],
-  display: 'swap',});
+  weight: ["400", "500", "700"],
+  style: ["normal", "italic"],
+  display: "swap",
+});
 
 export const metadata: Metadata = {
-  title: "colddsam",
-  description: "Welcome to my portfolio. Explore my projects, skills, achievements and experience in web development, machine learning, app development and problem solving. Contact me for collaborations and opportunities.",
+  title: "colddsam | Web Developer & Machine Learning Enthusiast",
+  description: "Discover colddsam's professional portfolio showcasing cutting-edge web development projects, innovative machine learning solutions, app development expertise, and problem-solving skills. Explore achievements, experience, and skills in full-stack development, graphics design, and more. Contact colddsam for collaborations and opportunities.",
   authors: [{ name: "colddsam", url: "https://github.com/colddsam" }],
-  keywords: ["colddsam", "web developer", "portfolio", "projects", "skills", "experience","achievements","projects","problem solving","machine learning","web development","app development","graphics designing", "contact"],
+  keywords: [
+    "colddsam", "web developer", "full-stack developer", "portfolio", "projects", 
+    "skills", "experience", "achievements", "problem-solving", "machine learning", 
+    "machine learning engineer", "web development", "app development", 
+    "mobile app development", "frontend development", "backend development", 
+    "graphics design", "software development", "full-stack development", 
+    "JavaScript", "React", "Node.js", "Python", "contact", "hire web developer", 
+    "hire machine learning engineer"
+  ],
   creator: "colddsam",
-
   icons: {
     icon: ['/images/profile/favicon.ico'],
     apple: ['/images/profile/apple-touch-icon.png'],
     shortcut: ['/images/profile/apple-touch-icon.png'],
-
-
   },
+  openGraph: {
+    type: 'website',
+    url: 'https://colddsam.xyz',
+    title: 'colddsam | Web Developer & Machine Learning Enthusiast',
+    description: "Discover colddsam's professional portfolio showcasing cutting-edge web development projects, innovative machine learning solutions, app development expertise, and problem-solving skills. Explore achievements, experience, and skills in full-stack development, graphics design, and more. Contact colddsam for collaborations and opportunities.",
+    images: [
+      {
+        url: '/images/profile/1200x630.png',
+        width: 1200,
+        height: 630,
+        alt: 'colddsam Portfolio',
+      },
+    ],
+  },
+  twitter: {
+    card: 'summary_large_image',
+    site: '@colddsam',
+    title: 'colddsam | Web Developer & Machine Learning Enthusiast',
+    description: 'Explore the portfolio of colddsam, showcasing projects, skills, achievements, and experience in web development, machine learning, app development, and problem-solving.',
+    images: ['/images/profile/1200x628.png'],
+  },
+  metadataBase: new URL("https://colddsam.xyz"),
+};
+
+export const viewport = {
+  width: "device-width",
+  initialScale: 1,
 };
 
 export default function RootLayout({
@@ -33,13 +66,18 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
-        <link rel="icon" href="/images/profile/favicon.ico'" sizes="any" />
-        <link
-            rel="icon"
-            href="/icon?<generated>"
-            type="image/<generated>"
-            sizes="<generated>"
-        />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" href="/images/profile/favicon.ico" sizes="any" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://colddsam.xyz" />
+        <meta property="og:title" content="colddsam | Web Developer & Machine Learning Enthusiast" />
+        <meta property="og:description" content="Discover colddsam's professional portfolio showcasing cutting-edge web development projects, innovative machine learning solutions, app development expertise, and problem-solving skills. Explore achievements, experience, and skills in full-stack development, graphics design, and more. Contact colddsam for collaborations and opportunities." />
+        <meta property="og:image" content="/images/profile/1200x630.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@colddsam" />
+        <meta name="twitter:title" content="colddsam | Web Developer & Machine Learning Enthusiast" />
+        <meta name="twitter:description" content="Explore the portfolio of colddsam, showcasing projects, skills, achievements, and experience in web development, machine learning, app development, and problem-solving." />
+        <meta name="twitter:image" content="/images/profile/1200x628.png" />
       </head>
       <body className={inter.className}>
         <Providers>
--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -23,7 +23,7 @@ const navItems = [
       icon: (
         <IconMail  className="h-4 w-4 text-white" />
       ),
-  },
+    },
     {
       name: "Gitub",
       link: "https://github.com/colddsam",
