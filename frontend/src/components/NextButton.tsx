39b8562e2daff530ae802730bd3e17a632e9b076
colddsam <dassamratkumar772@gmail.com>
2024-06-13 15:23:03 +0530

    modifying meeting link

--- frontend/src/components/NextButton.tsx
+++ frontend/src/components/NextButton.tsx
@@ -1,8 +1,5 @@
 "use client";
 import React from "react";
-import reactElementToJSXString from "react-element-to-jsx-string";
-// import { toast, Toaster } from "sonner";
-import { ButtonsCard } from "@/components/tailwindcss-buttons";
 
 export function TailwindcssButtons() {
   
--- frontend/src/components/tailwindcss-buttons.tsx
+++ frontend/src/components/tailwindcss-buttons.tsx
@@ -1,6 +1,5 @@
 "use client";
 import React from "react";
-import { IconClipboard } from "@tabler/icons-react";
 import { cn } from "@/utils/cn";
 
 export const ButtonsCard = ({
